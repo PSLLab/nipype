f1fc6c54cd0fc1abfb79d9709e88db89880fb22b
Jarrod Moss <jarrod.moss@psllab.org>
2016-10-24 15:48:42 -0500

    add aff2rigid FSL comman

--- nipype/interfaces/fsl/utils.py
+++ nipype/interfaces/fsl/utils.py
@@ -1101,6 +1101,21 @@ class PlotMotionParams(FSLCommand):
             return self._list_outputs()['out_file']
         return None
 
+class Affine2RigidInputSpec(FSLCommandInputSpec):
+    in_file = File(exists=True,
+                   desc='input filename',
+                   argstr='%s', position=0, mandatory=True)
+    out_matrix = File(desc="Output matrix name", argstr="%s", pos=1
+                   name_source=['in_file'], hash_files=False, mandatory=True
+                   name_template='%s_aff2rigid.mat', keep_extension=True)
+
+class Affine2RigidOutputSpec(TraitedSpec):
+    out_matrix = File(exists=True, desc="rigid output matrix")
+
+class Affine2Rigid(FSLCommand):
+    _cmd = 'aff2rigid'
+    input_spec = Affine2RigidInputSpec
+    output_spec = Affine2RigidOutputSpec
 
 class ConvertXFMInputSpec(FSLCommandInputSpec):
     in_file = File(exists=True, mandatory=True, argstr="%s", position=-1,
