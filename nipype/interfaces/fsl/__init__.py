b4c77a0952429febc75a5c78a7219f8b7a21590b
Jarrod Moss <jarrod.moss@psllab.org>
2016-10-25 08:35:39 -0500

    export Affine2Rigid

--- nipype/interfaces/fsl/__init__.py
+++ nipype/interfaces/fsl/__init__.py
@@ -17,7 +17,8 @@ from .utils import (Smooth, Merge, ExtractROI, Split, ImageMaths, ImageMeants,
                     PlotTimeSeries, PlotMotionParams, ConvertXFM,
                     SwapDimensions, PowerSpectrum, Reorient2Std,
                     Complex, InvWarp, WarpUtils, ConvertWarp, WarpPoints,
-                    WarpPointsToStd, RobustFOV, CopyGeom, MotionOutliers)
+                    WarpPointsToStd, RobustFOV, CopyGeom, MotionOutliers,
+                    Affine2Rigid)
 
 from .epi import (PrepareFieldmap, TOPUP, ApplyTOPUP, Eddy, EPIDeWarp,
                   SigLoss, EddyCorrect, EpiReg)
