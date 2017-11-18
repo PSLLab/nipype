375bf94b6eb540b91801b93a51f5705cf0984cf8
Jarrod Moss <jarrod.moss@psllab.org>
2017-11-18 10:54:40 -0600

    bug fix

--- nipype/interfaces/freesurfer/utils.py
+++ nipype/interfaces/freesurfer/utils.py
@@ -1023,39 +1023,39 @@ class MRIsConvert(FSCommand):
     input_spec = MRIsConvertInputSpec
     output_spec = MRIsConvertOutputSpec
 
-    def _format_arg(self, name, spec, value):
-        if name == "out_file" and not os.path.isabs(value):
-            value = os.path.abspath(value)
-        return super(MRIsConvert, self)._format_arg(name, spec, value)
+    # def _format_arg(self, name, spec, value):
+    #     if name == "out_file" and not os.path.isabs(value):
+    #         value = os.path.abspath(value)
+    #     return super(MRIsConvert, self)._format_arg(name, spec, value)
+    #
+    # def _list_outputs(self):
+    #     outputs = self.output_spec().get()
+    #     outputs["converted"] = os.path.abspath(self._gen_outfilename())
+    #     return outputs
 
-    def _list_outputs(self):
-        outputs = self.output_spec().get()
-        outputs["converted"] = os.path.abspath(self._gen_outfilename())
-        return outputs
-
-    def _gen_filename(self, name):
-        if name is 'out_file':
-            return os.path.abspath(self._gen_outfilename())
-        else:
-            return None
-
-    def _gen_outfilename(self):
-        if isdefined(self.inputs.out_file):
-            return self.inputs.out_file
-        elif isdefined(self.inputs.annot_file):
-            _, name, ext = split_filename(self.inputs.annot_file)
-        elif isdefined(self.inputs.parcstats_file):
-            _, name, ext = split_filename(self.inputs.parcstats_file)
-        elif isdefined(self.inputs.label_file):
-            _, name, ext = split_filename(self.inputs.label_file)
-        elif isdefined(self.inputs.scalarcurv_file):
-            _, name, ext = split_filename(self.inputs.scalarcurv_file)
-        elif isdefined(self.inputs.functional_file):
-            _, name, ext = split_filename(self.inputs.functional_file)
-        elif isdefined(self.inputs.in_file):
-            _, name, ext = split_filename(self.inputs.in_file)
-
-        return name + ext + "_converted." + self.inputs.out_datatype
+    # def _gen_filename(self, name):
+    #     if name is 'out_file':
+    #         return os.path.abspath(self._gen_outfilename())
+    #     else:
+    #         return None
+    #
+    # def _gen_outfilename(self):
+    #     if isdefined(self.inputs.out_file):
+    #         return self.inputs.out_file
+    #     elif isdefined(self.inputs.annot_file):
+    #         _, name, ext = split_filename(self.inputs.annot_file)
+    #     elif isdefined(self.inputs.parcstats_file):
+    #         _, name, ext = split_filename(self.inputs.parcstats_file)
+    #     elif isdefined(self.inputs.label_file):
+    #         _, name, ext = split_filename(self.inputs.label_file)
+    #     elif isdefined(self.inputs.scalarcurv_file):
+    #         _, name, ext = split_filename(self.inputs.scalarcurv_file)
+    #     elif isdefined(self.inputs.functional_file):
+    #         _, name, ext = split_filename(self.inputs.functional_file)
+    #     elif isdefined(self.inputs.in_file):
+    #         _, name, ext = split_filename(self.inputs.in_file)
+    #
+    #     return name + ext + "_converted." + self.inputs.out_datatype
 
 
 class MRITessellateInputSpec(FSTraitedSpec):
